3bdd916829627301c14024aec026769de5c750da
LindermanBR <linderman.moura@outlook.com>
2022-06-17 15:07:07 -0300

    Consulta simples com @Query

--- src/main/java/com/requiemdev/worshopmongo/repositories/PostRepository.java
+++ src/main/java/com/requiemdev/worshopmongo/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.requiemdev.worshopmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.requiemdev.worshopmongo.domain.Post;
@@ -11,6 +12,12 @@ import com.requiemdev.worshopmongo.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String>{
 //somente com isso ja é possível fazer varias operações com o user
 	
-	//Querry Method usando o spring para gerar consultas no mongo usando somente o titulo e parametros
+	//Query personalizada usando a anotação @querry o que permite usar o formato json
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
+	//Query Method usando o spring para gerar consultas no mongo usando somente o titulo e parametros
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	
 }
