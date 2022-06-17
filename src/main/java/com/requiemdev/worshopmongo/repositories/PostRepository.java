036ac9bba09ab3ea9fe834c3b31690d25e4f59ad
LindermanBR <linderman.moura@outlook.com>
2022-06-17 15:38:07 -0300

    Consulta com vários critérios

--- src/main/java/com/requiemdev/worshopmongo/repositories/PostRepository.java
+++ src/main/java/com/requiemdev/worshopmongo/repositories/PostRepository.java
@@ -1,5 +1,6 @@
 package com.requiemdev.worshopmongo.repositories;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -19,5 +20,7 @@ public interface PostRepository extends MongoRepository<Post, String>{
 	//Query Method usando o spring para gerar consultas no mongo usando somente o titulo e parametros
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
-	
+	//Query complexa
+	@Query("{ $and: [ { 'date': { $gte: ?1 } }, { 'date': { $lte: ?2 } } ,{ $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
