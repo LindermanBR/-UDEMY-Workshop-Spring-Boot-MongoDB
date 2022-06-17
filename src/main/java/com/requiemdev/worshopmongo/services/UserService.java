fef22f8f8ae871940e50188ee44333140e69bad7
LindermanBR <linderman.moura@outlook.com>
2022-06-16 21:37:49 -0300

    Obtendo um usuário por id

--- src/main/java/com/requiemdev/worshopmongo/services/UserService.java
+++ src/main/java/com/requiemdev/worshopmongo/services/UserService.java
@@ -1,21 +1,28 @@
 package com.requiemdev.worshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.requiemdev.worshopmongo.domain.User;
 import com.requiemdev.worshopmongo.repositories.UserRepository;
+import com.requiemdev.worshopmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
-	@Autowired //mecanismo de injeção de dependencia automatica do spring
+
+	@Autowired // mecanismo de injeção de dependencia automatica do spring
 	private UserRepository repo;
 
-	public List<User> findAll(){
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
 }
